d15b5ee61dcae3ec5239c7419b70e32e1483213c
adnrbp <adnrbp@users.noreply.github.com>
2019-10-08 21:59:30 -0500

    [eStore] Config: Update project to use mongo over docker-compose

--- estore/config/index.js
+++ estore/config/index.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 const config = {
 	dev: process.env.NODE_ENV !== 'production',
 	host: process.env.HOST || "0.0.0.0",
-	port: process.env.PORT || 3000
+	port: process.env.PORT || 3001,
+
+	cors: process.env.CORS,
+	dbUser: process.env.DB_USER,
+	dbPassword: process.env.DB_PASSWORD,
+	dbHost: process.env.DB_HOST,
+	dbName: process.env.DB_NAME
 }
 
 module.exports = { config };
